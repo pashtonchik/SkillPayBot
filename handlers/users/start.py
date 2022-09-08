331c579bf20c64593572a390e11bfc4f300092e5
kkiniuss <kkiniuss03@gmail.com>
2022-09-08 18:11:05 +0300

    fix

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -126,7 +126,7 @@ async def startJob(call: types.CallbackQuery):
             else:
                 await call.answer('Не удалось начать смену, свяжитесь с тех. поддержкой.', show_alert=True)
         else:
-            await call.answer("Вы и так уже не на смене!", show_alert=True)
+            await call.answer("Вы и так уже на смене!", show_alert=True)
 
 
 @dp.callback_query_handler(trade_cb.filter(action=['accept_trade']))
@@ -163,19 +163,23 @@ async def acceptOrder(call: types.CallbackQuery, callback_data: dict, state=FSMC
             }
             
             url = f'https://bitzlato.com/api/p2p/trade/{id}'
-
-            # req_change_type = requests.post(url, headers=headers, proxies=proxy, json=data)
-            # if (req_change_type.status_code == 200):
-            await call.message.edit_text(f'''
+            try:
+                req_change_type = requests.post(url, headers=headers, proxies=proxy, json=data)
+                
+                if (req_change_type.status_code == 200):
+                    await call.message.edit_text(f'''
     Переведите {get_current_info.json()['trade']['currency_amount']} {get_current_info.json()['trade']['currency']}
     Комментарий: {get_current_info.json()['trade']['details']}
     Реквизиты: {get_current_info.json()['trade']['counterDetails']} {get_current_info.json()['paymethod_description']}
 
-После перевода нажмите кнопку "Оплатил"
-''', reply_markup=kb_accept_payment)
-            await Activity.acceptOrder.set()
-            # else:
-                # await call.message.answer('Произошла ошибка')
+    После перевода нажмите кнопку "Оплатил"
+    ''', reply_markup=kb_accept_payment)
+                    await Activity.acceptOrder.set()
+                else:
+                    await call.answer('Произошла ошибка, нажмите кнопку заново.')
+            except Exception as e:
+                await call.answer('Произошла ошибка, нажмите кнопку заново.')
+                
         else:
             await call.answer("Заявка уже в работе", show_alert=True)
             await call.message.delete()
@@ -201,45 +205,53 @@ async def acceptPayment(call: types.CallbackQuery, callback_data=dict, state=FSM
     }
     
     url = f'https://bitzlato.bz/api/p2p/trade/{id}'
-
-    # req_change_type = requests.post(url, headers=headers, proxies=proxy, json=data)
+    try:
     
-    await call.message.answer('Пришлите чек о переводе в виде изображения.')
-    
-    # if (req_change_type.status_code == 200):
-    #     await call.message.answer('Пришлите чек о переводе в виде изображения.')
-    await state.update_data(id=id)
-    await Activity.acceptPayment.set()
-    # else:
-    #     await call.message.answer('Произошла ошибка в боте, сообщените админу')
-
+        req_change_type = requests.post(url, headers=headers, proxies=proxy, json=data)
+        
+        if (req_change_type.status_code == 200):
+            await call.message.answer('Пришлите чек о переводе в виде изображения.')
+            await state.update_data(id=id)
+            await Activity.acceptPayment.set()
+        else:
+            await call.message.answer('Произошла ошибка, нажмите кнопку заново.')
+    except Exception as e:
+        await call.message.answer('Произошла ошибка, нажмите кнопку заново.')
+        
 @dp.message_handler(content_types=['photo'], state=Activity.acceptPayment)
 async def getPhoto(message: types.Message, state=FSMContext):
+    URL_DJANGO = 'http://194.58.92.160:8000/'
     id = await state.get_data()
     print(id['id'])
     id = id['id']
+    get_trade_detail = requests.get(URL_DJANGO + f'api/trade/detail/{id}')
+
+    key = get_trade_detail.json()['user']['key']
+    proxy = get_trade_detail.json()['user']['proxy']
+    email = get_trade_detail.json()['user']['email']
+
     fileName = f'/root/SkillPayBot/media/{id}_{message.from_user.id}.png'
     await message.photo[-1].download(fileName)
-    # send_message = f'https://bitzlato.bz/api/p2p/trade/{tradeId}/chat/'
-    # headers = authorization()
-    # data_message = {
-    #     'message' : 'Оплатил.',
-    #     'payload' : {
-    #         'message' : 'string'
-    #     }
-    # }
-    # send_message_req = requests.post(send_message, headers=header, proxies=proxy, json=data_message)
-    # url = f'https://bitzlato.bz/api2/p2p/trade/{id}/chat/sendfile'
+    send_message = f'https://bitzlato.bz/api/p2p/trade/{id}/chat/'
+    headers = authorization(key, email)
+    data_message = {
+        'message' : 'Оплатил.',
+        'payload' : {
+            'message' : 'string'
+        }
+    }
+    send_message_req = requests.post(send_message, headers=header, proxies=proxy, json=data_message)
+    url = f'https://bitzlato.bz/api2/p2p/trade/{id}/chat/sendfile'
     
-    # data = {
-    #     'mime_type': 'image/png',
-    #     'name': 'Check.png'
-    # }
-    # files = {'file': open(fileName, 'rb')}
+    data = {
+        'mime_type': 'image/png',
+        'name': 'Check.png'
+    }
+    files = {'file': open(fileName, 'rb')}
     
-    # headers = authorization()
+    headers = authorization()
     
-    # r = requests.post(url, headers=headers, proxies=proxies, files=files)
+    r = requests.post(url, headers=headers, proxies=proxy, files=files)
 
     
     body = {

f98825691c215bf70aa543fbe615191938d27451
pashtonchik <pashka191@yandex.ru>
2022-11-09 04:08:03 +0300

    asdasdsa

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -579,12 +579,12 @@ async def get_photo(message: types.Message, state=FSMContext):
         print(auth)
         jwt = get_jwt(uid=auth['uid'], private_key=auth['private_key'])
 
-        file_name = f'/root/prod/SkillPay-Django/kf_checks/gar{id}_{message.from_user.id}.pdf'
+        file_name = f'/root/prod/SkillPay-Django/gar_checks/gar{id}_{message.from_user.id}.pdf'
         if message.content_type == 'document' and message.document.file_name[-3:] == 'pdf':
             await message.document.download(destination_file=file_name)
             data = {
                 'id': id,
-                'cheque': f'kf_checks/gar{id}_{message.from_user.id}.pdf'
+                'cheque': f'gar_checks/gar{id}_{message.from_user.id}.pdf'
             }
             upload = requests.post(URL_DJANGO + 'update/garantex/trade/', json=data)
 

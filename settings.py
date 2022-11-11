52bbbbef0add788d99b13aef05b7fb2bf7c679b7
kkiniuss <kkiniuss03@gmail.com>
2022-11-12 00:58:35 +0300

    fix

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 URL_DJANGO = 'http://194.58.92.160:8000/api/'
 URL_BZ = 'https://bitzlato.com/'
-cheques_base = '/root/dev/SkillPay-Django/kf_checks/'
+cheques_base = '/root/prod/SkillPay-Django/kf_checks/'

b9316e60eb80e0f0f1683239dd74ed6d479a6971
kkiniuss <kkiniuss03@gmail.com>
2022-11-13 11:30:26 +0300

    Refactor25/100

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -309,7 +309,7 @@ async def accept_order(call: types.CallbackQuery, callback_data: dict, state=FSM
                     elif (url_type == 'pay'):
                         type = 'googleSheets'
                     await Activity.acceptPayment.set()
-                    await state.update_data(id=trade_id, type=type, message_id=msg.message_id)
+                    await state.set_data(id=trade_id, type=type, message_id=msg.message_id)
                 except Exception as e:
                     await call.answer('Произошла ошибка, нажмите кнопку заново.')
 
@@ -438,7 +438,7 @@ async def accept_order(call: types.CallbackQuery, callback_data: dict, state=FSM
                         type = 'googleSheets'
                     
                     await Activity.acceptPayment.set()
-                    await state.update_data(id=trade_id, type=type, message_id=msg.message_id)
+                    await state.set_data(id=trade_id, type=type, message_id=msg.message_id)
                 except Exception as e:
                     print(e)
                     await call.answer('Произошла ошибка, нажмите кнопку заново.')

c4a77e55cb6a8f23a5dc5ae828c58392d7b0590d
kkiniuss <kkiniuss03@gmail.com>
2022-11-20 12:10:03 +0300

    fix

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -857,7 +857,7 @@ async def get_photo(message: types.Message, state=FSMContext):
 Заявка: GAR — {id}
 Инструмент: {trade_detail['gar_trade']['paymethod']}
 –––
-Сумма: `{trade_detail['gar_trade']['currency_amount']}` 
+Сумма: `{trade_detail['gar_trade']['amount']}` 
 –––
 Адресат: `{trade_detail['gar_trade']['details']}`
 –––

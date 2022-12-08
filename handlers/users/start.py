bc757d77dd080e33dbbe623faa5811a0c513e5cb
kkiniuss <kkiniuss03@gmail.com>
2022-12-08 03:43:09 +0300

    fix

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -267,9 +267,7 @@ async def accept_order(call: types.CallbackQuery, callback_data: dict, state=FSM
                 trade_type = 'gar_trade'
 
             if url_type in ['gar', 'pay', 'kf', 'bz']:
-                print(trade_id)    
                 get_pay_info = requests.get(URL_DJANGO + f'{url_type}/trade/detail/{trade_id}/')
-                print(get_pay_info)
                 if (not get_pay_info.json()[trade_type]['agent'] or str(
                         get_pay_info.json()[trade_type]['agent']) == str(
                     call.from_user.id)) and \

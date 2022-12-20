016d941f9f4e36c19df3400024a8094a7cc1ee32
pashtonchik <pashka191@yandex.ru>
2022-12-21 02:50:41 +0300

    asdasdas

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1150,8 +1150,8 @@ async def get_photo(message: types.Message, state=FSMContext):
                                     agent = get_agent_info_req.json()[0]['user_name']
                                 print(get_current_info.json())
                                 t = get_current_info.json()[trade_type]
-                                await bot.edit_message_text(chat_id=CHANNEL_ID, message_id=t['channel_message_id'],
-                                                            text=f'🟢 {t["platform_id"]} : {paymethod[t["paymethod"]]} : {t["amount"]} : {data["user_name"]}')
+                                # await bot.edit_message_text(chat_id=CHANNEL_ID, message_id=t['channel_message_id'],
+                                #                             text=f'🟢 {t["platform_id"]} : {paymethod[t["paymethod"]]} : {t["amount"]} : {data["user_name"]}')
 
                                 # msg = await message.answer("Обновление баланса🆙", reply_markup=reply_markup=update_keyboard(data['income_operator'], "Начать смену"))
                             else:
